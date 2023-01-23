b0240e39b47314ea98368c36b33651a9151b5834
GOWTHAMAN R <pudukkottai>
2023-01-23 18:37:07 +0530

    basic svelte file created

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<main>
+  <h1 class="text-center text-4xl text-red-500">Hello My Team!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+</main>
+
+<style global lang="postcss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
