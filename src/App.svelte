b8c55b00df856aa7be6fefabea6b8b7124c68736
GOWTHAMAN R <pudukkottai>
2023-01-26 19:05:52 +0530

    folder dlted

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Index from "./FileTemplateEditor/index.svelte"
+import Index from "./FileTemplateEditor/index.svelte"
 </script>
 
 <main>
@@ -7,17 +7,17 @@
 </main>
 
 <style global lang="postcss">
-  @tailwind base;
-  @tailwind components;
-  @tailwind utilities;
-  .spanLabelName {
-    @apply select-none text-sm font-bold text-textGray group-focus-within:text-primary_blue;
-  }
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+.spanLabelName {
+  @apply select-none text-sm font-bold text-textGray group-focus-within:text-primary_blue;
+}
 
-  .label-div {
-    @apply absolute inset-y-0 left-3 flex items-center;
-  }
-  .inputbox {
-    @apply block h-10 w-3/4 rounded-md border-none bg-lightGray text-sm text-white focus:ring-2  focus:ring-primary_blue;
-  }
+.label-div {
+  @apply absolute inset-y-0 left-3 flex items-center;
+}
+.inputbox {
+  @apply block h-10 w-3/4 rounded-md border-none bg-lightGray text-sm text-white focus:ring-2  focus:ring-primary_blue;
+}
 </style>
