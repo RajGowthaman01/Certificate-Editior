b50c3d238d365f06f067707d18093135606c7a2a
venky-123-debug <venky26494@yahoo.com>
2023-01-26 17:21:02 +0530

    colors updated in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,6 +40,26 @@ module.exports = {
         input_white: "rgb(242,242,242)",
         place_white: "rgb(180,180,180)", //text-color
         icon_black: "rgb(66,66,66)",
+        secondary: "#1e1e1e", //black for majority
+        primary: "#444549", //text-color for label
+        primaryGray: "#949494",
+        topNavSvgBg: "#4e4f51",
+        heading: "#fcfefb", //properties white
+        certificateSection: "#252525",
+        secondaryGray: "#666769", //for text
+        primaryBlue: "#007acd", //vscode blue
+        hr: "gray-700/50",
+        roundedRed: "#f93925",
+        Orange: "#f97f1a",
+        Yellow: "#f9c921",
+        SkyBlue: "#2fedd0",
+        DarkBlue: "#1dacf0",
+        green: "#3bb143",
+        Red: "#ef4444",
+        rose: "#e8155c",
+      },
+       boxShadow: {
+        box_Shadow: "0px 0px 20px 10px rgb(0 0 0 / .15)",
       },
     },
   },
