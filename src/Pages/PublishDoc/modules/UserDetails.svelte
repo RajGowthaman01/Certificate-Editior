a67eed991b0706090b5e29655cb5d935d510a90d
GOWTHAMAN R <pudukkottai>
2023-02-02 20:28:52 +0530

    venkatesh bro folder added

--- src/Pages/PublishDoc/modules/UserDetails.svelte
+++ src/Pages/PublishDoc/modules/UserDetails.svelte
@@ -18,9 +18,9 @@
           clip-rule="evenodd"
         />
       </svg>
-      <p class="overflow-hidden text-ellipsis whitespace-nowrap font-mono">819f82006a4c49263fcde49372eb58589194cc759fcc2c8758d804f97021cbe3</p>
+      <span class="overflow-hidden text-textGray text-ellipsis whitespace-nowrap font-mono">819f82006a4c49263fcde49372eb58589194cc759fcc2c8758d804f97021cbe3</span>
     </div>
-    <div class="bg-lightGray rounded-sm p-2">
+    <div class="bg-lightGray text-textGray rounded-sm p-2">
       <div class="flex gap-3 items-center">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="fill-textGray bg-darkGray rounded-sm w-24 h-24">
           <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd" />
@@ -36,14 +36,14 @@
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="fill-textGray w-5 h-5">
             <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clip-rule="evenodd" />
           </svg>
-          <p class="font-mono">9876543210</p>
+          <span class="font-mono">9876543210</span>
         </div>
         <div class="flex gap-3 items-center pt-2">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="fill-textGray w-5 h-5">
             <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
             <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
           </svg>
-          <p>john@email.com</p>
+          <span>john@email.com</span>
         </div>
       </div>
     </div>
---
+++ src/Pages/components/imagePreview.svelte
@@ -0,0 +1,20 @@
+<div class="flex flex-col">
+	<div class="flex space-x-4 bg-secondary px-4 py-4">
+		<div class="canvas" id="canvas">
+			<img src="assets/images/down1.png" alt="" class="h-20 w-20 flex-none rounded-md" />
+		</div>
+		<div class="mt-2 flex-auto space-y-1">
+			<p class="imgProps">SAMPLE 1</p>
+			<p class="imgProps">1280 X 720Px</p>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.canvas {
+		@apply h-20 w-20 flex-none rounded-md bg-gray-100 object-contain;
+	}
+	.imgProps {
+		@apply text-sm font-medium text-heading;
+	}
+</style>
