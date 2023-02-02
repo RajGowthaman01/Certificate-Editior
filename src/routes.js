00109b197365cff9c7460bb6fc9ce6189336d3ee
GOWTHAMAN R <pudukkottai>
2023-02-02 19:59:19 +0530

    karthi code added

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import Analytics from "./Pages/Analytics/index.svelte"
 import PublishDoc from "./Pages/PublishDoc/index.svelte"
 import FileTemplateEditor from "./Pages/FileTemplateEditor/Index.svelte"
-import DarkFileTemplateEditor from "./Pages/DarkFileTemplateEditor/index.svelte"
-import TemplateEditorOnSlide from "./Pages/TemplateEditorOnSlide/index.svelte"
+// import DarkFileTemplateEditor from "./Pages/DarkFileTemplateEditor/index.svelte"
+// import TemplateEditorOnSlide from "./Pages/TemplateEditorOnSlide/index.svelte"
 export default {
   "/": PublishDoc,
   "/Analytics": Analytics,
   "/FileTemplateEditor": FileTemplateEditor,
-  "/DarkFileTemplateEditor": DarkFileTemplateEditor,
-  "/TemplateEditorOnSlide": TemplateEditorOnSlide,
+  // "/DarkFileTemplateEditor": DarkFileTemplateEditor,
+  // "/TemplateEditorOnSlide": TemplateEditorOnSlide,
   // The catch-all route must always be last
   "*": Analytics,
 }
