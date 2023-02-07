1d24aa42d48626ebc18464a4081d43fb12d32a5d
GOWTHAMAN R <pudukkottai>
2023-02-07 10:37:41 +0530

    signOevrlay5 created

--- src/Pages/SignerOverlay/Components/UserDetails.svelte
+++ src/Pages/SignerOverlay/Components/UserDetails.svelte
@@ -108,7 +108,7 @@
 </div> -->
 
 <!--card3-->
-<div class="pb-5">
+<!-- <div class="pb-5">
   {#if loadScreen}
     <LoadingScreen />
   {:else}
@@ -130,7 +130,6 @@
             clip-rule="evenodd"
           />
         </svg>
-        <!-- <span class="overflow-hidden text-textGray text-ellipsis whitespace-nowrap font-mono">819f82006a4c49263fcde49372eb58589194cc759fcc2c8758d804f97021cbe3</span> -->
         <span class="text-textGray break-all font-mono">819f82006a4c49263fcde49372eb58589194cc759fcc2c8758d804f97021cbe3</span>
       </div>
       <span>Signer Who works in the company named as Test Company Pvt. Ltd</span>
@@ -153,7 +152,45 @@
         </svg>
         <span class="text-sm">john@email.com</span>
       </div>
-      <!-- <button on:click={gotoNextSignStep} class="btn mt-5 ml-auto">next</button> -->
+    </div>
+  {/if}
+</div> -->
+
+<!--card4-->
+<div class="pb-5">
+  {#if loadScreen}
+    <LoadingScreen />
+  {:else}
+    <div class="text-textGray flex flex-col gap-1">
+      <div class="flex gap-3 items-center mb-2">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="fill-darkGray bg-lightGray w-12 h-12 rounded-full">
+          <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd" />
+        </svg>
+        <div>
+          <h1 class="text-2xl text-textGray font-bold">John Doe</h1>
+          <h6 class="text-xs text-textGray font-thin">Designation</h6>
+        </div>
+      </div>
+      <span class="text-sm ">Test Company Pvt. Ltd, Chennai</span>
+      <span class="text-textGray text-sm break-all font-mono">ID: 819f82006a4c49263fcde49372eb58589194cc759fcc2c8758d804f97021cbe3</span>
+
+      <!-- <div class="flex gap-2 items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
+          <path fill-rule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
+        </svg>
+        <p>Chennai</p>
+      </div> -->
+      <div class="flex gap-2 items-center mt-3">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="fill-textGray cursor-pointer w-4 h-4">
+          <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clip-rule="evenodd" />
+        </svg>
+        <!-- <span class="font-mono text-sm">9876543210</span> -->
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="fill-textGray cursor-pointer w-4 h-4">
+          <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
+          <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
+        </svg>
+        <!-- <span class="text-sm">john@email.com</span> -->
+      </div>
     </div>
   {/if}
 </div>
--- src/Pages/SignerOverlay/index.svelte
+++ src/Pages/SignerOverlay/index.svelte
@@ -3,9 +3,11 @@
   import SignerOverlay2 from "./modules/SignerOverlay2.svelte"
   import SignerOverlay3 from "./modules/SignerOverlay3.svelte"
   import SignerOverlay4 from "./modules/SignerOverlay4.svelte"
+  import SignerOverlay5 from "./modules/SignerOverlay5.svelte"
 </script>
 
 <!-- <SignerOverlay /> -->
 <!-- <SignerOverlay2 /> -->
 <!-- <SignerOverlay3 /> -->
-<SignerOverlay4 />
+<!-- <SignerOverlay4 /> -->
+<SignerOverlay5 />
