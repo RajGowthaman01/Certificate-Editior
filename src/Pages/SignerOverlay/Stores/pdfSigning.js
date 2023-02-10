bf12027a3db2004034c2e80d3baac062c400638d
GOWTHAMAN R <pudukkottai>
2023-02-10 18:19:02 +0530

    jsDocs wrote for stores

--- src/Pages/SignerOverlay/Stores/pdfSigning.js
+++ src/Pages/SignerOverlay/Stores/pdfSigning.js
@@ -1,6 +1,16 @@
 import { writable } from "svelte/store"
 
 const pdfSigning = writable({
+  /**
+   * @typedef {Object} signerDetails - signer details from api call
+   * @property {String} companyName - signer company name
+   * @property {String} name - signer name
+   * @property {String} Designation - signer designation
+   * @property {Number} phone - signer contact number
+   * @property {String} eMail - signer mail id
+   * @property {String} id - signer id
+   */
+  /**@type {signerDetails} */
   signerDetails: {
     companyName: "Test Company Pvt. Ltd", //company name of the signer
     name: "John Doe", //signer name
@@ -9,10 +19,22 @@ const pdfSigning = writable({
     eMail: "john@email.com", //signer mail id
     id: "819f82006a4c49263fcde49372eb58589194cc759fcc2c8758d804f97021cbe3", //signer id
   },
+
+  /**
+   * @property {Number} pageNo - signer selected page number
+   * @property {Boolean} horizontal - signer selected page number
+   */
+  /**@type {signPlacement} */
   signPlacement: {
     pageNo: 0, //selected page number
     horizontal: true, //initally the sign placement is horizontally lock
   },
+
+  /**
+   * @typedef {Object} signColor - contain signer selected signatue background colour
+   * @property {String} color - signer selected colour
+   */
+  /**@type {signColor} */
   signColor: {
     color: "#242424", //background colour for the signature
   },
