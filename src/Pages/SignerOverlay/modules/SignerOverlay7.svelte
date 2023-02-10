92dbc788bd2e561d148fb4656948da680ea128b5
Mathavan <118589467+Mathav495@users.noreply.github.com>
2023-02-10 11:42:13 +0530

    signer overlay placed in publishdoc page

--- src/Pages/SignerOverlay/modules/SignerOverlay7.svelte
+++ src/Pages/SignerOverlay/modules/SignerOverlay7.svelte
@@ -62,14 +62,14 @@
     loadScreen = true
   }
 
-  $: transition = "translate-y-44"
+  $: transition = "translate-y-40"
   const gotoNextSignStep = (id) => {
     if (id == 0) {
-      transition = "translate-y-[5.7rem]"
+      transition = "translate-y-[4.8rem]"
     } else if (id == 1) {
-      transition = "translate-y-2"
+      transition = "-translate-y-2"
     } else if (id == 2) {
-      transition = "-translate-y-20"
+      transition = "-translate-y-24"
     }
 
     loadScreen = true
@@ -92,7 +92,7 @@
 
   const gotoBackSignStep = (id) => {
     if (id == 1) {
-      transition = "translate-y-44"
+      transition = "translate-y-40"
     } else if (id == 2) {
       transition = "translate-y-[5.7rem]"
     } else if (id == 3) {
@@ -115,75 +115,75 @@
   }
 </script>
 
-<main class="h-screen w-screen flex justify-center items-center">
+<!-- <main class="h-screen w-screen flex justify-center items-center">
   <div class="grid grid-cols-12 w-full h-full bg-lightGray">
     <div class="col-span-3 flex" />
     <div class="col-span-9 h-full flex">
-      <div class="w-8/12" />
-      <div class="w-4/12 relative h-full">
-        <div class="w-full h-full flex items-center">
-          <div class="pl-5 {transition} transition ease-in-out duration-500 ">
-            <ol class="overflow-hidden">
-              {#each sections as section}
-                <li class="relative pb-12">
-                  <div class="absolute{sections[sections.length - 1] == section ? 'hidden' : ''} {section.complete ? 'bg-primary_blue' : 'bg-darkGray'} top-4 left-2 -ml-px mt-0.5 h-full w-0.1 opacity-50" />
-                  {#if (section.Active && !section.complete) || (section.Active && section.complete)}
-                    <!-- Complete Step -->
-                    <div class="group relative flex items-center justify-center" aria-current="step">
-                      <span class="flex h-9 items-center" aria-hidden="true">
-                        <span class="relative z-10 flex h-4 w-4 items-center justify-center rounded-full bg-darkGray">
-                          <span class="h-2 w-2 rounded-full animate-ping bg-primary_blue" />
-                        </span>
-                      </span>
-                    </div>
-                  {:else if !section.Active && section.complete}
-                    <!-- Current Step -->
-                    <div class="group relative flex items-center justify-center">
-                      <span class="flex h-9 items-center">
-                        <span class="relative z-10 flex h-4 w-4 items-center justify-center rounded-full bg-primary_blue" />
-                      </span>
-                    </div>
-                  {:else if !section.Active && !section.complete}
-                    <!-- Upcoming Step -->
-                    <div class="group relative flex items-center justify-center">
-                      <span class="flex h-9 items-center">
-                        <span class="relative z-10 flex h-2 w-2 items-center justify-center rounded-full border-2 border-darkGray bg-darkGray" />
-                      </span>
-                    </div>
-                  {/if}
-                </li>
-              {/each}
-            </ol>
-          </div>
+      <div class="w-8/12" /> -->
+<div class="w-full relative h-full">
+  <h1 class="text-center absolute top-0 w-full text-base bg-primary_blue text-white py-3">Document Signature Section</h1>
 
-          <div class="w-4 h-4 ml-2 mt-12 z-20 rounded-sm rotate-45 bg-darkGray" />
-          <div class="relative -ml-2 mr-5 w-full h-full flex flex-col justify-center">
-            <h1 class="text-center absolute top-0 w-full text-xl bg-primary_blue text-white py-4">Document Signature Section</h1>
+  <div class="w-full h-full flex items-center">
+    <div class="px-2 {transition} transition ease-in-out h-80 duration-500 ">
+      <ol class="overflow-hidden">
+        {#each sections as section}
+          <li class="relative pb-12">
+            <div class="absolute{sections[sections.length - 1] == section ? 'hidden' : ''} {section.complete ? 'bg-primary_blue' : 'bg-Analytics-sidebar'} top-4 left-2.5 -ml-px mt-0.5 h-full w-0.1 opacity-50" />
+            {#if (section.Active && !section.complete) || (section.Active && section.complete)}
+              <!-- Complete Step -->
+              <div class="group relative flex items-center justify-center" aria-current="step">
+                <span class="flex h-9 items-center" aria-hidden="true">
+                  <span class="relative z-10 flex h-5 w-5 items-center justify-center rounded-full bg-Analytics-sidebar">
+                    <span class="h-3 w-3 rounded-full animate-ping bg-primary_blue" />
+                  </span>
+                </span>
+              </div>
+            {:else if !section.Active && section.complete}
+              <!-- Current Step -->
+              <div class="group relative flex items-center justify-center">
+                <span class="flex h-9 items-center">
+                  <span class="relative z-10 flex h-5 w-5 items-center justify-center rounded-full bg-primary_blue" />
+                </span>
+              </div>
+            {:else if !section.Active && !section.complete}
+              <!-- Upcoming Step -->
+              <div class="group relative flex items-center justify-center">
+                <span class="flex h-9 items-center">
+                  <span class="relative z-10 flex h-3 w-3 items-center justify-center rounded-full bg-Analytics-sidebar" />
+                </span>
+              </div>
+            {/if}
+          </li>
+        {/each}
+      </ol>
+    </div>
 
-            <div class="w-full mt-12 relative">
-              {#each sections as section (section.id)}
-                {#if section.Active}
-                  <div class="w-full min-h-[250px] h-full bg-darkGray flex-col rounded-md">
-                    <!-- svelte-ignore a11y-click-events-have-key-events -->
-                    <div class="text-base text-white w-full font-bold cursor-pointer text-center py-4">{section.Title}</div>
+    <div class="w-4 h-4 pl-2 mt-8 z-20 rounded-sm rotate-45 bg-Analytics-secondary" />
+    <div class="relative -ml-2 pr-5  w-full h-full flex flex-col justify-center">
+      <div class="w-full pt-4 relative">
+        {#each sections as section (section.id)}
+          {#if section.Active}
+            <div class="w-full min-h-[250px] h-full bg-Analytics-secondary flex-col rounded-md">
+              <!-- svelte-ignore a11y-click-events-have-key-events -->
+              <div class="text-base text-white w-full font-bold cursor-pointer text-center py-4">{section.Title}</div>
 
-                    <div in:fade class="w-full min-h-[250px] relative my-auto px-4 pb-4 ">
-                      <svelte:component this={section.Component} on:ShowImg={showImg} on:init={() => gotoNextSignStep(section.id)} {loadScreen} on:nextStep={() => gotoNextSignStep(section.id)} />
-                    </div>
-                    {#if sections[0] != section}
-                      <button on:click={() => gotoBackSignStep(section.id)} class="btn absolute bottom-4 left-4">
-                        <BackIcon />
-                      </button>
-                    {/if}
-                  </div>
-                {/if}
-              {/each}
+              <div in:fade class="w-full min-h-[250px] relative my-auto px-4 pb-4 ">
+                <svelte:component this={section.Component} on:ShowImg={showImg} on:init={() => gotoNextSignStep(section.id)} {loadScreen} on:nextStep={() => gotoNextSignStep(section.id)} />
+              </div>
+              {#if sections[0] != section}
+                <button on:click={() => gotoBackSignStep(section.id)} class="btn absolute bottom-4 left-4">
+                  <BackIcon />
+                </button>
+              {/if}
             </div>
-          </div>
-        </div>
+          {/if}
+        {/each}
       </div>
     </div>
   </div>
+</div>
+<!-- </div>
+  </div> -->
 
-  <!-- <Empty /> -->
-</main>
+<!-- <Empty /> -->
+<!-- </main> -->
